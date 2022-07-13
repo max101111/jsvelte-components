3da56ff1c9be165065f40d36fd0e59db09d1117a
Jerry <jerry.liao@weihongtech.cn>
2022-07-13 17:36:24 +0800

    test

--- src/lib/table/Table.stories.svelte
+++ src/lib/table/Table.stories.svelte
@@ -3,26 +3,24 @@
 	import Table from './Table.svelte';
 </script>
 
-
 <!-- 👇 Creates specific parameters for the story -->
-<Meta
-	title="Components/Table"
-	component={Table}
-	argTypes={{
-		name: { control: 'text' },
-		textColor: { control: 'color' },
-	}}
-/>
+<Meta title="Components/Table" component={Table} argTypes={{}} />
 
 <Template let:args>
 	<Table {...args} />
 </Template>
 
-
 <Story
-	name="Jane"
+	name="Default"
 	args={{
-		name: 'Jane',
-		textColor: 'rgb(173, 216, 230)',
+		data: [
+			{ name: 456, job: 'qq', color: 'red' },
+			{ name: 456, job: 'qq', color: 'red' },
+		],
+		column: [
+			{ dataIndex: 'name', title: 'NAME' },
+			{ dataIndex: 'job', title: 'JOB' },
+			{ dataIndex: 'color', title: 'Favorite Color' },
+		],
 	}}
 />
--- src/lib/table/Table.svelte
+++ src/lib/table/Table.svelte
@@ -1,39 +1,26 @@
 <script>
+	export let data = [];
+	export let column = [];
 </script>
 
 <div class="overflow-x-auto">
-  <table class="table w-full">
-    <!-- head -->
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-      </tr>
-    </thead>
-    <tbody>
-      <!-- row 1 -->
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      <!-- row 2 -->
-      <tr>
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      <!-- row 3 -->
-      <tr>
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
-    </tbody>
-  </table>
+	<table class="table w-full">
+		<!-- head -->
+		<thead>
+			<tr>
+				{#each column as item}
+					<th>{item.title || ''}</th>
+				{/each}
+			</tr>
+		</thead>
+		<tbody>
+			{#each data as item}
+				<tr>
+					{#each column as it}
+						<td>{item[it.dataIndex] || ''}</td>
+					{/each}
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 </div>
